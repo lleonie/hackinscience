70d1fbaae88d07b1b67db4079ecb7dec759b769f
Léonie Ledrein <leonie.ledrein@cri-paris.org>
2015-10-22 11:06:28 +0200

    25 exercise

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -3,4 +3,4 @@ from datetime import time
 date = datetime.date.today()
 now = datetime.datetime.now().time()
 heure = now.replace(microsecond=0)
-print("Today is %s and it is %s " % (date, heure))
+print("Today is %s and it is %s" % (date, heure))
