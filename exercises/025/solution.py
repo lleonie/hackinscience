aab5afa138e97732292e775252842fbe4c9ed430
Léonie Ledrein <leonie.ledrein@cri-paris.org>
2015-10-22 10:55:25 +0200

    25 exercise

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,6 +1,5 @@
-import datetime 
+import datetime
 from datetime import time
 date = datetime.date.today()
 heure = datetime.datetime.now().time()
 print("Today is %s and it is %s " % (date, heure))
-
