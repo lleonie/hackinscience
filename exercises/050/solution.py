82d4da0483cc13b1338e95b7177d46f66b76191f
Léonie Ledrein <leonie.ledrein@cri-paris.org>
2015-10-30 21:05:51 +0100

    50 exercise

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,7 +1,7 @@
 def sum(x):
     s = 0
     for number in x:
-        if number % 3 and number % 5:
+        if not number % 3 or not number % 5:
             s += number
     print(s)
-sum(range(0, 1000))
+sum(range(1000))
