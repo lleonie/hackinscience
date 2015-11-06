bf29bbde4d86ee7a60550888decf704284b8e5c0
Léonie Ledrein <leonie.ledrein@cri-paris.org>
2015-11-06 16:43:18 +0100

    70 exercises

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -1,5 +1,6 @@
 import string
 alpha = string.lowercase
 for n in range(0, len(alpha)):
-    for m in range(1, len(alpha) -1):
-        print( "%s%s" % (alpha[n], alpha[m]))
+    for m in range(0, len(alpha)):
+        if n != m:
+            print( "%s%s" % (alpha[n], alpha[m]))
