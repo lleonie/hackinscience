80e204d614cf13bebd04b4d8758856a78dff28a3
Léonie Ledrein <leonie.ledrein@cri-paris.org>
2015-11-06 17:03:13 +0100

    70 exercise

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -3,4 +3,4 @@ alpha = string.ascii_lowercase
 for n in range(0, len(alpha)):
     for m in range(0, len(alpha)):
         if n != m:
-            print( "%s%s" % (alpha[n], alpha[m]))
+            print("%s%s" % (alpha[n], alpha[m]))
